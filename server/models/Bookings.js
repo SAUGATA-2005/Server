00b8ee2e3006bad32e8e9818e0cd2ea098db1046
SAUGATA KUMAR JANA <saugata.23bce8398@vitapstudent.ac.in>
2025-07-09 22:43:49 +0530

    Create Bookings.js

---
+++ server/models/Bookings.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  customerPhone: { type: String, required: true },
+  restaurantName: { type: String, required: true },
+  city: { type: String, required: true },
+  date: { type: String, required: true }, // You can also use Date type
+  menu: [
+    {
+      name: String,
+      price: Number
+    }
+  ]
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
