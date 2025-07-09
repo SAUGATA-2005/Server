08d9afabedb48b087eec0536c6f9be185e942e11
SAUGATA KUMAR JANA <saugata.23bce8398@vitapstudent.ac.in>
2025-07-10 01:58:13 +0530

    Update Booking.js

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
   customerPhone: { type: String, required: true },
+  customerName: { type: String, required: true },
   restaurantName: { type: String, required: true },
   city: { type: String, required: true },
   date: { type: String, required: true }, // You can also use Date type
